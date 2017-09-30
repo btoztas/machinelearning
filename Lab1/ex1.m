cdaf29c849ea2b143a95730dc48572ef4c1bffad
afonsocosta8 <afonside@hotmail.com>
2017-09-30 19:32:33 +0100

    error calc

--- Lab1/ex1.m
+++ Lab1/ex1.m
@@ -4,6 +4,12 @@ Beta = inv(X'*X)*X'*y;
 yt = Beta(1) + Beta(2)*xt;
 scatter(y,x);
 hold on;
-plot (yt,xt);
-syms k;
-SSE = symsum((y(k) - Beta(1) - Beta(2)*x(k))^2, k, 1, 20);
+plot(yt,xt);
+SSE = 0;
+%syms k;
+%SSE = symsum((y(k) - Beta(1) -Beta(2)*x(k))^2, k, 1, 20);
+%SSE = symsum(k^2, k, 1, 10);
+for k=1:20
+    p = (y(k) - Beta(1) -Beta(2)*x(k))^2;
+    SSE = SSE + p;
+end
