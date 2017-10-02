282ab2ab14d269552c9293c913cc8781ccfbe256
Bruno Gonçalves <btoztas@gmail.com>
2017-10-02 15:14:46 +0100

    added some comments

--- Lab1/ex1.m
+++ Lab1/ex1.m
@@ -1,10 +1,18 @@
+%% Loading data files
+load('mfiles1/data1.mat');
+
+%% Creating X matrix and Fingind beta0 and beta1
 X = [ones(20,1), x];
-xt = -1:0.1:1;
 Beta = inv(X'*X)*X'*y;
+
+%% Ploting training data and fit
+xt = -1:0.1:1;
 yt = Beta(1) + Beta(2)*xt;
 scatter(y,x);
 hold on;
 plot(yt,xt);
+
+%% Finding error
 SSE = 0;
 %syms k;
 %SSE = symsum((y(k) - Beta(1) -Beta(2)*x(k))^2, k, 1, 20);
